c7aaa8d47f0ec39e6447a782f9657b8dde6518c3
getupminaaa <getupmina0405@gmail.com>
2021-05-05 18:08:16 +0900

    email, name UI set

--- Willing/app/src/main/java/com/teamgether/willing/Fragment/MyPageFragment.kt
+++ Willing/app/src/main/java/com/teamgether/willing/Fragment/MyPageFragment.kt
@@ -6,30 +6,52 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.Query
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
-import com.teamgether.willing.Adapter.RankingAdapter
+import com.google.firebase.firestore.ktx.toObjects
 import com.teamgether.willing.R
-import com.teamgether.willing.model.Ranking
+import com.teamgether.willing.model.UserInfo
+import kotlinx.android.synthetic.main.fragment_my_page.*
 
 class MyPageFragment : Fragment() {
-    
+
+    val user: FirebaseUser = FirebaseAuth.getInstance().currentUser
+    private var db = FirebaseFirestore.getInstance()
+    var userInfo = UserInfo()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         return inflater.inflate(R.layout.fragment_my_page, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val email = user.email
+        val imageUrl = ""
+        val name = userInfo.name
+        getUserData(email)
 
+    }
 
+    fun getUserData(email: String) {
+        db.collection("User").whereEqualTo("email", email).get().addOnSuccessListener { documents ->
+            for (document in documents) {
+                userInfo.email = user.email
+                val userdata = documents.toObjects<UserInfo>()
+                userInfo = userdata[0]
+
+                mp_email.setText(userInfo.email)
+                mp_nickName.setText(userInfo.name)
+            }
+        }.addOnFailureListener { exception ->
+            Log.w("TAG", "Error getting documents: ", exception)
+        }
+    }
+    fun getFollowerCount(){
 
     }
 }
