af8089be037cfb906042a7f838383bc4d8a170d7
getupminaaa <getupmina0405@gmail.com>
2021-05-30 20:34:20 +0900

    follow /unfollow 기능 구현 완료

--- Willing/app/src/main/java/com/teamgether/willing/Fragment/UserProfileFragment.kt
+++ Willing/app/src/main/java/com/teamgether/willing/Fragment/UserProfileFragment.kt
@@ -3,16 +3,20 @@ package com.teamgether.willing.Fragment
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.collection.LLRBNode
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import com.teamgether.willing.Adapter.ChallengeListAdapter
@@ -26,17 +30,25 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import javax.xml.parsers.ParserConfigurationException
 
 class UserProfileFragment : Fragment() {
     private lateinit var activity: Activity
     private lateinit var list: ArrayList<ChallengeList>
+
     private var auth = FirebaseAuth.getInstance()
+
     private var db = FirebaseFirestore.getInstance()
     private val storage: FirebaseStorage =
         FirebaseStorage.getInstance("gs://willing-88271.appspot.com/")
+
     private val currentUser = auth.currentUser
     private var userEmail: String? = ""
+    private var currentUserName: String = ""
+    private var userName: String = ""
     var isMine: Boolean = false
+    private var isFollow = false
+
     var profileInfo: ProfileInfo = ProfileInfo(
         profileImg = "",
         name = "",
@@ -50,12 +62,7 @@ class UserProfileFragment : Fragment() {
     )
 
     private lateinit var challengeListAdapter: ChallengeListAdapter
-    val name = "name"
-    val email = "email"
-    val tobe = "tobe"
-    val profileImg = "profileImg"
-    private val FOLLOWER = "Follower"
-    private val FOLLOWING = "Following"
+
     var profileImgUrl: String = ""
 
     override fun onAttach(context: Context) {
@@ -75,9 +82,11 @@ class UserProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         userEmail = this.arguments?.getString("userEmail")
+        Log.d("TAG", "argument: $userEmail")
 //        userEmail = currentUser?.email // 만약 값이 넘어오면 여기다가 다시 덮어씌울 것.
+        userEmail = "mr_magnet@naver.com"
         //값을 받는 부분 여기에~~~~~~~~
-//        userEmail = "skycat0212@jejunu.ac.kr"
+
 
         if (userEmail.equals(currentUser?.email)) {
             isMine = true
@@ -89,6 +98,7 @@ class UserProfileFragment : Fragment() {
 
         } else {
             mp_profile_update_btn.isVisible = false
+            mp_menu_btn.isVisible = false
             mp_title.setText(R.string.up_title)
         } //팔로잉버튼 숨기기
         showLoadingDialog()
@@ -105,7 +115,14 @@ class UserProfileFragment : Fragment() {
                 context?.startActivity(this)
             }
         }
+        mp_follow_btn.setOnClickListener {
 
+            if (isFollow) {
+                unfollowUser()
+            } else {
+                followUser()
+            }
+        }
         refresh_layout.setOnRefreshListener {
             getChallenge()
             challengeListAdapter.notifyDataSetChanged()
@@ -113,6 +130,15 @@ class UserProfileFragment : Fragment() {
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+        Log.d("TAG", "onStart: ${profileInfo.email}")
+        showLoadingDialog()
+        getUserData()
+        Log.d("TAG", "onStart: ${profileInfo.profileImg}")
+
+        getChallenge()
+    }
 
     private fun getProfileImg(
         data: String,
@@ -143,11 +169,17 @@ class UserProfileFragment : Fragment() {
     }
 
     private fun getUserData() {
+        val nameField = "name"
+        val emailField = "email"
+        val tobeField = "tobe"
+        val profileImg = "profileImg"
+
+
         db.collection("User").whereEqualTo("email", userEmail).get()
             .addOnSuccessListener { result ->
                 val document = result.documents[0]
-                profileInfo.name = document[name] as String
-                profileInfo.email = document[email] as String
+                profileInfo.name = document[nameField] as String
+                profileInfo.email = document[emailField] as String
                 profileInfo.profileImg = document[profileImg] as String
                 if (currentUser != null) {
                     profileInfo.isMine = true
@@ -160,10 +192,9 @@ class UserProfileFragment : Fragment() {
 
                 getProfileImg(profileInfo.profileImg.toString(), mp_profile_img, this)
                 Log.d("TAG", "getUserData: ${profileInfo.profileImg}")
+                getFollowData(userEmail.toString())
+                getFollowStatus()
 
-
-                getFollowFollowingData(FOLLOWER, userEmail)
-                getFollowFollowingData(FOLLOWING, userEmail)
 
             }.addOnFailureListener { exception ->
                 Log.w("TAG", "Error getting documents: ", exception)
@@ -175,12 +206,12 @@ class UserProfileFragment : Fragment() {
         val subjectField = "subject"
         val titleField = "title"
         val percentField = "percent"
-        db.collection("Challenge").whereEqualTo("UID", userEmail).get()
+        db.collection("Challenge").whereEqualTo("uid", userEmail).get()
             .addOnSuccessListener { result ->
                 list = arrayListOf()
                 for (document in result) {
                     val challenges = ChallengeList()
-                    val documentId = document.id as String
+                    val documentId = document.id
                     val subject = document[subjectField] as String
                     val title = document[titleField] as String
                     val percent = document[percentField] as Number
@@ -200,42 +231,21 @@ class UserProfileFragment : Fragment() {
             }
     }
 
-    private fun getFollowFollowingData(collectionName: String, email: String?) {
-        db.collection(collectionName).document(email.toString()).get()
-            .addOnSuccessListener { result ->
-                if (collectionName.equals(FOLLOWER)) {
-                    val follow = result["follower"] as ArrayList<*>?
-                    if (follow.isNullOrEmpty()) {
-                        mp_follow_user.text = "0"
-                    } else {
-                        mp_follow_user.text = follow.size.toString()
-                    }
+    private fun getFollowData(email: String) {
+        db.collection("Follow").document(email).get().addOnSuccessListener { result ->
+            val follower = result["follower"] as ArrayList<*>?
+            val following = result["following"] as ArrayList<*>?
+            if (follower.isNullOrEmpty()) {
+                mp_follower_user.text = "0"
+            } else mp_follower_user.text = follower.size.toString()
+            if (following.isNullOrEmpty()) {
+                mp_following_user.text = "0"
+            } else mp_following_user.text = following.size.toString()
+        }
 
-                } else {
-                    val following = result["following"] as ArrayList<*>?
-                    if (following.isNullOrEmpty()) {
-                        mp_following_user.text = "0"
-                    } else {
-                        mp_following_user.text = following.size.toString()
-                    }
-                }
-            }
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.d("TAG", "onStart: ${profileInfo.email}")
-        showLoadingDialog()
-        getUserData()
-        Log.d("TAG", "onStart: ${profileInfo.profileImg}")
 
-        getChallenge()
-    }
-
-    //profile 수정 시 갤러리, 촬영 등으로 이동할 수 있도록 구현 필요
-    //프로필 수정 상태에 따른 변수 flag 세워서 저장 버튼 visible 관리 하기
-    //콜백 처리 하든 순서를 앞으로 당기든 로딩 늦는거 해결하기
-    // 로딩중 구현
     private fun showLoadingDialog() {
         val dialog = LoadingDialog(activity)
         CoroutineScope(Dispatchers.Main).launch {
@@ -245,4 +255,107 @@ class UserProfileFragment : Fragment() {
         }
     }
 
-}
+    //follow 상태를 가져오는 거 필요
+    private fun getFollowStatus() {
+        userName = profileInfo.name
+        db.collection("Follow").document(currentUser?.email.toString()).get()
+            .addOnSuccessListener { result ->
+                val following = result["following"] as ArrayList<*>?
+                isFollow = following?.contains(userName) == true
+                if (isFollow) {
+                    mp_follow_btn.setText(R.string.mp_did_follow)
+                    mp_follow_btn.setBackgroundColor(Color.parseColor("#F0F1F2"))
+                } else {
+                    mp_follow_btn.setText(R.string.mp_not_follow)
+                    mp_follow_btn.setBackgroundColor(Color.parseColor("#5AC5DF"))
+                }
+            }
+        //userEmail의 name이 내 following 필드에 있는지를 검사
+    }
+
+    private fun getCurrentUserName() {
+        db.collection("User").whereEqualTo("email", currentUser?.email).get()
+            .addOnSuccessListener { result ->
+                val document = result.documents[0]
+                currentUserName = document["name"] as String
+            }
+    }
+
+    private fun followUser() {
+        getCurrentUserName()
+        userName = profileInfo.name
+        var followerList: MutableList<String> = ArrayList()
+        var followingList: MutableList<String> = ArrayList()
+        db.collection("Follow").document(currentUser?.email.toString()).get()
+            .addOnSuccessListener { result ->
+                followingList = result["following"] as ArrayList<String>
+                Log.d("TAG", "followUserListBF: $followingList")
+                followingList.add(userName)
+                Log.d("TAG", "followUserListAF: $followingList")
+                db.collection("Follow").document(currentUser?.email.toString())
+                    .update("following", followingList).addOnSuccessListener {
+                    Log.d("TAG", "followingUser: 성공성공성공")
+                    getFollowData(userEmail.toString())
+                    getFollowStatus()
+                }
+            }
+
+        db.collection("Follow").document(userEmail.toString()).get()
+            .addOnSuccessListener { result ->
+                followerList = result["follower"] as ArrayList<String>
+                Log.d("TAG", "followUserBF: $followerList")
+                followerList.add(currentUserName)
+                Log.d("TAG", "followUserAF: $followerList")
+                db.collection("Follow").document(userEmail.toString())
+                    .update("follower", followerList).addOnSuccessListener {
+                    Log.d("TAG", "followerUser: 성공성공성공")
+                    getFollowData(userEmail.toString())
+                    getFollowStatus()
+                }
+            }
+
+//        db.collection("Follow").document(userEmail.toString()).update("follower"))
+        //그 사람의 follower에 내 이름이 추가
+        //내 following에 그 사람 이름이 추가
+    }
+
+    private fun unfollowUser() {
+
+        getCurrentUserName()
+        userName = profileInfo.name
+        var followerList: MutableList<String> = ArrayList()
+        var followingList: MutableList<String> = ArrayList()
+        db.collection("Follow").document(currentUser?.email.toString()).get()
+            .addOnSuccessListener { result ->
+                followingList = result["following"] as ArrayList<String>
+                Log.d("TAG", "followUserListBF: $followingList")
+                followingList.remove(userName)
+                Log.d("TAG", "followUserListAF: $followingList")
+                db.collection("Follow").document(currentUser?.email.toString())
+                    .update("following", followingList).addOnSuccessListener {
+                        Log.d("TAG", "followingUser: 성공성공성공")
+                        getFollowData(userEmail.toString())
+                        getFollowStatus()
+                    }
+            }
+
+        db.collection("Follow").document(userEmail.toString()).get()
+            .addOnSuccessListener { result ->
+                followerList = result["follower"] as ArrayList<String>
+                Log.d("TAG", "followUserBF: $followerList")
+                followerList.remove(currentUserName)
+                Log.d("TAG", "followUserAF: $followerList")
+                db.collection("Follow").document(userEmail.toString())
+                    .update("follower", followerList).addOnSuccessListener {
+                        Log.d("TAG", "followerUser: 성공성공성공")
+                        getFollowData(userEmail.toString())
+                        getFollowStatus()
+                    }
+            }
+        getFollowData(userEmail.toString())
+        getFollowStatus()
+    }
+    //following -> 내가 팔로우 하는 거
+    //follower -> 다른 사람이 나한테 한 거
+    //follow -> following하는 행위
+}//challenge 비공개인거 안불러오는 구문 추가
